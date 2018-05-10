18b971e56090bdecce2c74b89112adf2aa5ac345
FilipRebizant <filiprebizant@gmail.com>
2018-05-10 14:45:29 +0200

    added basic view for home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Hall;
+use App\Entity\Movie;
+use App\Entity\Screening;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,8 +17,13 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+
+        $movies = $this->getDoctrine()->getRepository(Movie::class)->findAll();
+        $screenings = $this->getDoctrine()->getRepository(Screening::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'screenings' => $screenings,
+            'movies' => $movies
         ]);
     }
 }
