e156c3c34ca68c342fd39d651167400d1a0bf2ea
Artugam <artguam@gmailc.com>
2018-05-24 19:46:50 +0200

    File upload edit

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -15,89 +15,84 @@ use Symfony\Component\HttpFoundation\File\File;
 /**
  * @Route("/movie")
  */
-class MovieController extends Controller
-{
+class MovieController extends Controller {
+
     /**
      * @Route("/", name="movie_index", methods="GET")
      */
-    public function index(MovieRepository $movieRepository): Response
-    {
+    public function index(MovieRepository $movieRepository): Response {
         return $this->render('movie/index.html.twig', ['movies' => $movieRepository->findAll()]);
-    }
+        }
 
-    /**
-     * @Route("/new", name="movie_new", methods="GET|POST")
-     */
-    public function new(Request $request, FileUploader $fileUploader): Response
-    {
+        /**
+         * @Route("/new", name="movie_new", methods="GET|POST")
+         */
+        public function new(Request $request, FileUploader $fileUploader): Response
+        {
         $movie = new Movie();
         $form = $this->createForm(MovieType::class, $movie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
-            $file = $movie->getImage();
-            $fileName = $fileUploader->upload($file);
 
-            $movie->setImage($fileName);
-            
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($movie);
-            $em->flush();
+        $file = $movie->getImage();
+        $fileName = $fileUploader->upload($file);
+        $movie->setImage($fileName);
 
-            return $this->redirectToRoute('movie_index');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($movie);
+        $em->flush();
+
+        return $this->redirectToRoute('movie_index');
         }
 
         return $this->render('movie/new.html.twig', [
-            'movie' => $movie,
-            'form' => $form->createView(),
+                    'movie' => $movie,
+                    'form' => $form->createView(),
         ]);
     }
 
     /**
      * @Route("/{id}", name="movie_show", methods="GET")
      */
-    public function show(Movie $movie): Response
-    {
+    public function show(Movie $movie): Response {
         return $this->render('movie/show.html.twig', ['movie' => $movie]);
     }
 
     /**
      * @Route("/{id}/edit", name="movie_edit", methods="GET|POST")
      */
-    public function edit(Request $request, Movie $movie, FileUploader $fileUploader): Response
-    {
-      
-        $movie->setImage(
-            new File($this->getParameter('images_directory').'/'.$movie->getImage())
-        );
+    public function edit(Request $request, Movie $movie, FileUploader $fileUploader): Response {
+
+        if ($movie->getImage()) {
+            $movie->setImage(
+                    new File($this->getParameter('images_directory') . '/' . $movie->getImage())
+            );
+        }
         $form = $this->createForm(MovieType::class, $movie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
-            $file = $movie->getImage();
-            $fileName = $fileUploader->upload($file);
-            $movie->setImage($fileName);
-            
-            $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('movie_edit', ['id' => $movie->getId()]);
+            $fileName = $fileUploader->upload($movie->getImage());
+            $movie->setImage($fileName);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($movie);
+            $em->flush();
+            return $this->redirectToRoute('movie_index');
         }
-        
-        
+
         return $this->render('movie/edit.html.twig', [
-            'movie' => $movie,
-            'form' => $form->createView(),
+                    'movie' => $movie,
+                    'form' => $form->createView(),
         ]);
     }
 
     /**
      * @Route("/{id}", name="movie_delete", methods="DELETE")
      */
-    public function delete(Request $request, Movie $movie): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$movie->getId(), $request->request->get('_token'))) {
+    public function delete(Request $request, Movie $movie): Response {
+        if ($this->isCsrfTokenValid('delete' . $movie->getId(), $request->request->get('_token'))) {
             $em = $this->getDoctrine()->getManager();
             $em->remove($movie);
             $em->flush();
@@ -105,5 +100,5 @@ class MovieController extends Controller
 
         return $this->redirectToRoute('movie_index');
     }
-    
+
 }
